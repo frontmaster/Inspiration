c69d58a1a7cf05dcae4f592511725de74223a784
shun-front <>
2021-12-18 12:55:08 +0900

    npm run production実行

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,6 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('postidealist-component', require('./components/PostIdeaListComponent.vue').default);
 Vue.component('ideadlist-component', require('./components/IdeaListComponent.vue').default);
 Vue.component('likeidealist-component', require('./components/LikeIdeaListComponent.vue').default);
