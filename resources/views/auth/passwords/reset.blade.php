54f78d37564ae6dd4565c10f2de646442a95128e
shun-front <>
2022-01-16 23:48:02 +0900

    修正

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -52,7 +52,7 @@
                 <div class="form-group row mb-0">
                     <div class="col-md-6 offset-md-4">
                         <button type="submit" class="btn btn-primary">
-                            送信
+                            {{ __('Reset Password') }}
                         </button>
                     </div>
                 </div>
