bcfed5bfbd8164486aef0e92760c4087357546c9
shun-front <>
2022-01-17 00:08:07 +0900

    修正

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -23,31 +23,35 @@
                 @enderror
                 <div class="p-passEdit__part">
                     <label for="email" class="p-passEdit__label">{{ __('E-Mail Address') }}</label>
-                    <input type="email" class="p-passEdit__input @error('email') is-error @enderror" name="email" value="{{ $email ?? old('email') }}">
+                    <input id="email" type="email" class="p-passEdit__input @error('email') is-error @enderror" name="email" value="{{ $email ?? old('email') }}">
                 </div>
 
                 @error('new_password')
-                <span class="c-errMsg p-passEdit__errMsg">
-                    <p>{{ $message }}</p>
-                </span>
-                @enderror
+                        <span class="c-errMsg p-passEdit__errMsg">
+                            <p>{{ $message }}</p>
+                        </span>
+                        @enderror
                 <div class="p-passEdit__part">
                     <label for="new_password" class="p-passEdit__label">{{ __('Password') }}</label>
-                    <input type="password" class="p-passEdit__input @error('new_password') is-error @enderror" name="new_password">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
                 </div>
 
-                <div class="p-passEdit__part">
-                    <label for="password-confirm" class="p-passEdit__label">{{ __('Confirm Password') }}</label>
-                    <input id="password-confirm" type="password" class="p-passEdit__input" name="password_confirmation">
+                <div class="form-group row">
+                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                    <div class="col-md-6">
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    </div>
                 </div>
 
-                
-                    <div class="p-passEdit__button">
-                        <button type="submit" class="c-btn p-passEdit__btn">
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
                             {{ __('Reset Password') }}
                         </button>
                     </div>
-                
+                </div>
             </form>
 
         </div>
