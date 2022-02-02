afa037b03eb107b1a58cc36195a85728c8b3133f
shun-front <>
2022-02-02 15:25:12 +0900

    退会処理

--- app/Http/Controllers/PostIdeasController.php
+++ app/Http/Controllers/PostIdeasController.php
@@ -128,7 +128,7 @@ class PostIdeasController extends Controller
         $idea_id = optional($postidea)->id;
         $already_liked = Like::where('user_id', $user_id)->where('idea_id', $idea_id)->first();
         $review = IdeaReview::where('post_idea_id', $id)->where('post_user_id', $user_id)->first();
-        if ($postidea) {
+        if ($postidea && $postidea->user != null) {
             $postIdeaUser = $postidea->user()->first();
             $category = $postidea->category()->first();
             $ideaReview = IdeaReview::where('post_idea_id', $id)->with('user')->get();
