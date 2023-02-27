b33a48394893f896bd4263fd9839478f7a72a695
shun-front <>
2023-02-27 21:37:00 +0900

    AppServiceProvider編集

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         Schema::defaultStringLength(191);
-        $url->forceScheme('https'); 
     }
 }
